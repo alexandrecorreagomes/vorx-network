0ebaf9de081622965a06355086c9218f30a678eb
Alexandre Gomes <alexandrecorreagomes@gmail.com>
2023-02-15 21:03:43 -0300

    Add: Create subnets priv and pub

--- network.tf
+++ network.tf
@@ -6,6 +6,26 @@ resource "aws_vpc" "vorx-vpc-prod" {
     }
 }
 
+resource "aws_subnet" "vorx-subnet-pub-1a" {
+    vpc_id = aws_vpc.vorx-vpc-prod.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "prod-public-subnet-1a"
+    }
+}
+
+resource "aws_subnet" "vorx-subnet-priv-1a" {
+    vpc_id = aws_vpc.vorx-vpc-prod.id
+    cidr_block = "10.0.10.0/24"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "prod-private-subnet-1a"
+    }
+}
+
 # Outpus
 output "vpc_vorx_prod_id" {
     value = aws_vpc.vorx-vpc-prod.id
@@ -13,4 +33,12 @@ output "vpc_vorx_prod_id" {
 
 output "vpc_vorx_prod_arn" {
     value = aws_vpc.vorx-vpc-prod.arn
+}
+
+output "vorx_subnet_pub_1a_id" {
+    value = aws_subnet.vorx-subnet-pub-1a.id
+}
+
+output "vorx_subnet_priv_1a_id" {
+    value = aws_subnet.vorx-subnet-priv-1a.id
 }
